ebfae82561df81571596443907e5a91d152df5fd
Kaustubh pythoneer <73011416+kaustubh-alt@users.noreply.github.com>
2022-12-25 17:06:53 +0530

    Update US stock info.py

--- US stock info.py
+++ US stock info.py
@@ -1,6 +1,9 @@
 #https://www.alphavantage.co/support/#api-key
 #go to aboove link for free api key
-from alpha_vantage.timeseries import TimeSeries
+#you need an api key for access to US stock 
+
+
+from alpha_vantage.timeseries import TimeSeries #you also need to install package 'ALPHAVANTAGE' to run this code
 import pandas as pd
 APIkey = #Enter your API key here
 ts = TimeSeries(key=APIkey, output_format='pandas')
